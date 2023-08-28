fdcef24f6011984828068ea64ba688f3e9a09fd0
Guilherme Salgueiro Silveira <127560908+guilhermesilveiras@users.noreply.github.com>
2023-08-28 00:49:45 -0300

    Update and rename media2 to media2.js
    O primeiro código informa a situação do aluno com base na média de três notas previamente inseridas. O segundo código informa quanto o aluno precisa em uma eventual terceira prova para passar com a média 7.

---
+++ media2.js
@@ -0,0 +1,23 @@
+
+// código 1
+
+var nota1 = parseFloat(prompt("Digite a primeira nota:"));
+var nota2 = parseFloat(prompt("Digite a segunda nota:"));
+var nota3 = parseFloat(prompt("Digite a terceira nota:"));
+
+var media = (nota1 + nota2 + nota3) / 3;
+
+// média estabelecida = 7
+var situacao = media < 7 ? "reprovado" : "aprovado";
+
+console.log("O aluno está", situacao, "com média", media.toFixed(2));
+
+// código 2
+
+var nota1 = parseFloat(prompt("Digite a primeira nota:"));
+var nota2 = parseFloat(prompt("Digite a segunda nota:"));
+
+var mediaAtual = (nota1 + nota2) / 2;
+var notaMinima = (7 * 3) - (nota1 + nota2);
+
+console.log("Para passar com média sete, você precisa tirar no mínimo:", notaMinima.toFixed(2));
