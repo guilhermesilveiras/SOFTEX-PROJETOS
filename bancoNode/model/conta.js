233d183ca826c6962544d68ab4680f677c07b3f7
guilhermesilveiras <127560908+guilhermesilveiras@users.noreply.github.com>
2023-09-20 11:25:14 -0300

    att 2

---
+++ bancoNode/model/conta.js
@@ -0,0 +1,42 @@
+import { Transacao } from "./transacao.js";
+import { TIPOTRANS } from "./tipoTransacao.js";
+
+export class Conta{
+    constructor(cliente, numero, agencia, saldo){
+        this.cliente = cliente;
+        this.numero = numero;
+        this.agencia = agencia;
+        this.dataCriacao = Date.now();
+        this.saldo = saldo;
+        this.status = true;
+        this.transacoes = [];
+    }
+
+    //depositar na conta
+    depositar(valor){
+        this.saldo += valor;
+        let trans = new Transacao(TIPOTRANS, deposito, new Date().toLocaleDateString(), valor, null, '+');
+        this.transacoes.push(trans);
+    }
+
+    //sacar na conta
+    sacar(valor){
+        if (this.saldo>=valor) {
+            this.saldo -= valor;
+            let trans = new Transacao(TIPOTRANS.debito, new Date().toLocaleDateString(), valor, null, '-');
+            this.transacoes.push(trans);
+        } else {
+            //lançar um erro
+            console.log('Erro: saldo insuficiente' , valor + 'maior que o saldo' + this.saldo + '-'); 
+            
+        }
+    }
+
+    //transferir da conta
+
+    //pagamento da conta
+
+    //saldo da conta
+
+    //extrato da conta
+}
---
+++ bancoNode/model/tipoTransacao.js
@@ -0,0 +1,6 @@
+export const TIPOTRANS = {
+    debito : 'DÉBITO',
+    deposito : 'DEPÓSITO',
+    transferencia : 'TRANSFERÊNCIA',
+    pagamento : 'PAGAMENTO'
+}
