2cc071987583239496681792e10db9634d3fef95
guilhermesilveiras <127560908+guilhermesilveiras@users.noreply.github.com>
2023-11-01 20:25:31 -0300

    Update app.ts

--- TS/expFactoryMethod/src/app.ts
+++ TS/expFactoryMethod/src/app.ts
@@ -20,3 +20,8 @@ let pcSoftex = FabricaComp.criadorComputador(3.2, 8, 256, "PC", 2500);
 (pcSoftex as PC).setMonitor("LG 24UHD");
 
 console.log(pcSoftex?.toString());
+
+let servidorSoftex2 = Object.create(servidorSoftex)
+servidorSoftex2.setSistemaRAID("RAID 7");
+servidorSoftex2.setSO("Ubuntu");
+servidorSoftex2.CPU
